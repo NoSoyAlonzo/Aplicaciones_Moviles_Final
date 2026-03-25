7f2ad2613faee234a25b023edd3b0a153b41f09c
Javier <javier.castillo239873@potros.itson.edu.mx>
2026-03-24 22:02:10 -0700

    Clase explorar

---
+++ app/src/main/java/com/example/proyectoapp_moviles/Explorar.kt
@@ -0,0 +1,39 @@
+package com.example.proyectoapp_moviles
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Explorar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.explorar)
+
+        val explorar = findViewById<LinearLayout>(R.id.explorar)
+        val misMascotas = findViewById<LinearLayout>(R.id.mis_mascotas)
+        val reservas = findViewById<LinearLayout>(R.id.reservas)
+        val perfil = findViewById<LinearLayout>(R.id.mi_perfil)
+
+        explorar.setOnClickListener {
+            startActivity(Intent(this, Explorar::class.java))
+        }
+
+        misMascotas.setOnClickListener {
+            startActivity(Intent(this, ScreenHome::class.java))
+        }
+
+        reservas.setOnClickListener {
+            startActivity(Intent(this, Reservas::class.java))
+        }
+
+        perfil.setOnClickListener {
+            startActivity(Intent(this, PerfilActivity::class.java))
+        }
+
+    }
+
+}
